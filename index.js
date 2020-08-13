0818278d469f8f1f479a6d3f7826578773e112d7
BennaceurHichem <gh_bennaceur@esi.dz>
2020-08-13 02:48:38 +0100

    adding hbs views and connecting them with routers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express  =  require('express')
 const morgan  =  require('morgan')
 const dotenv  =  require('dotenv')
 const exphbs = require('express-handlebars')
-
+const path = require('path')
 //Load config formt he specific file config.env,
 // result will eb acdessible from process.env
 dotenv.config({ path: './config/config.env' })
@@ -13,9 +13,16 @@ if(process.env.NODE_ENV==="development")
      app.use(morgan('dev'))
  }
 
+
+ app.use(express.static(path.join(__dirname,'public')))
+
 //configure express-handlebars to use views in our folder with .hbs extension
- app.engine('.hbs', exphbs({extname: '.hbs'}));
+ app.engine('.hbs', exphbs({defaultLayout:'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//configure touters 
+  app.use('/', require('./routes/index.js'));
+
 
 const dbConnection  = require('./config/db')
 
