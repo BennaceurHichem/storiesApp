e27d890a2a1611f52cbde0944d8ff2c7082ab8e0
BennaceurHichem <gh_bennaceur@esi.dz>
2020-08-31 11:29:21 +0100

    try to solve cross env problem in heroku

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const MongoStore = require('connect-mongo')(session)
 const methodOverride = require('method-override')
 
 
+//netlify 
+const serverless = require('serverless-http')
+
+
 
 //Load config formt he specific file config.env,
 // result will eb acdessible from process.env
@@ -99,9 +103,9 @@ const {formatDate,stripTags,editIcon,select,truncate} = require('./helpers/hbs')
 app.set('view engine', '.hbs');
 
 //configure routers 
-  app.use('/', require('./routes/index.js'));
-  app.use('/auth', require('./routes/auth.js'));
-  app.use('/stories', require('./routes/stories.js'));
+  app.use('app', require('./routes/index.js'));
+  app.use('auth', require('./routes/auth.js'));
+  app.use('stories', require('./routes/stories.js'));
 
 
  
@@ -117,7 +121,7 @@ dbConnection()
 
 
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3001
 
 
 
@@ -125,3 +129,9 @@ app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT} in ${process.env.NODE_ENV} mode `)
   })
   
+  
+
+
+
+ 
+// module.exports.handler  = serverless(app)
